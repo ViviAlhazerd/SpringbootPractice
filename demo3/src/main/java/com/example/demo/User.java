48709c0d5923d8afd0053dd00ab8837122866194
ViviAlhazerd <65544128+ViviAlhazerd@users.noreply.github.com>
2024-03-14 14:39:38 +0900

    end

---
+++ demo3/src/main/java/com/example/demo/User.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String address;
+    @Column
+    private String tel;
+}
